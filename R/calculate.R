0e0ca1aaa1106cd3640762263a261ab650117ed5
Gabriel Bogo <gabrielmedeirosbogo@gmail.com>
2019-03-02 10:38:30 -0800

    moved .git directory and other files to new package project folder

--- R/calculate.R
+++ R/calculate.R
@@ -1,16 +1,26 @@
+#' Title
+#'
+#' @param x
+#' @param stat
+#'
+#' @return
+#' @export
+#'
+#' @examples
 calculate <- function(x,stat){
+
   #Calculate function takes a tibble containing the resampled groups and groups them and calculates a summary statistic for each group
   #For this milestone, we've chosen to go with the mean as the statistic
-  
+
   #Parameters
-  
+
   # x: tibble
   #Tibble containing all resampled observations
   # stat: string
   #Specifies what type of summary statistic is to be calculated. For now, only mean is available.
-  
+
   #Outputs
-  
+
   #Tibble containing the summarized data, one for each resampled group (n_samples specified in generate)
-  
+
 }
